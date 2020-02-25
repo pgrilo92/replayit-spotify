9c53de906b0a4a1f112e59efd5216450b6890de3
Joaquim <pgrilo92@hotmail.com>
2020-02-25 11:55:45 -0500

    change uri

--- src/Services/config.js
+++ src/Services/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "23707be4c54c4530a0edd2e5c1adef67";
-export const redirectUri = "https://pgrilo92.github.io/replayit-spotify";
+export const redirectUri = "https://pgrilo92.github.io/replayit-spotify/callback/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
